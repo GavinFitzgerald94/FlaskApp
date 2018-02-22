1e3403f1b96017343259bfa29b7eb09b3b896ffe
Gavin <gavin.fitzgerald@ucdconnect.ie>
2018-02-22 00:54:01 +0000

    fix

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from app import app
 from FindSystemInformation.main import main
 import os
 os.system("export FLASK_APP=/Users/gavin/Desktop/Semester2/comp30670/FlaskApp")
+os.system("flask run")
 @app.route('/')
 def index():
         returnDict = {}
